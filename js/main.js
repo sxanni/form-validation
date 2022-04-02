06e8eb499746238f8b89fd578648214d27a95e1c
sanni <opeyemisanni65@gmail.com>
2022-04-01 23:49:47 -0400

    form valid2

--- js/main.js
+++ js/main.js
@@ -73,6 +73,6 @@ function verifyPassword(){
     let pw1 = document.getElementById("password").value;  
     let pw2 = document.getElementById("password2").value;  
     if(pw1!==pw2){
-        alert("password not matching")
+        alert("password does not match")
     }
 } 
